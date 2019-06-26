bd239f386f7eee53d60ae96495ea7b96bb09e583
gustavomm <gustavomm22.gm@gmail.com>
2019-06-26 09:24:07 -0300

    FutureBuilder e input funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ import "dart:convert";
 const request = "https://api.hgbrasil.com/finance?key=b3549cf3";
 
 void main() {
-  runApp(MaterialApp(home: Home(),));
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
 Future<Map> getData() async {
@@ -20,8 +22,63 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("\$ Conversor de Moedas \$"),
+          centerTitle: true,
+          backgroundColor: Colors.amberAccent,
+        ),
+        backgroundColor: Colors.black12,
+        body: FutureBuilder(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return Center(
+                    child: Text("Carregando Dados...",
+                        style: TextStyle(color: Colors.white70, fontSize: 25),
+                        textAlign: TextAlign.center),
+                  );
+                default:
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Erro ao Carregar Dados :(",
+                          style: TextStyle(color: Colors.white70, fontSize: 25),
+                          textAlign: TextAlign.center),
+                    );
+                  } else {
+                    dolar =
+                    snapshot.data["results"]["currencies"]["USD"]["buy"];
+                    euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                    return SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Icon(Icons.monetization_on, size: 150,
+                                color: Colors.amberAccent),
+                            TextField(
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                labelText: "Reais",
+                                labelStyle: TextStyle(
+                                  color: Colors.amberAccent,
+                                ),
+                                border: OutlineInputBorder(),
+                                prefixText: "R\$"
+                              ),
+                            )
+                          ],
+                        )
+                    );
+                  }
+              }
+            }));
   }
 }
